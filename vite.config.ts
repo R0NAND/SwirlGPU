ffcbb28f63b831df0371cf817099b3fc7cf4ecf3
Ronan Tunney <rjtunney@edu.uwaterloo.ca>
2025-05-01 00:39:50 -0400

    deploying build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import string from "vite-plugin-string";
 
 export default defineConfig({
+  build: {
+    target: "esnext", //browsers can handle the latest ES features
+  },
   plugins: [
     string({
       include: "**/*.wgsl",
